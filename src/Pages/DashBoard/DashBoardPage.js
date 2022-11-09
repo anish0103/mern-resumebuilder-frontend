04a6cb02dc3e70f0583286f98af58500c4e6b0c7
Anish Patel <anish.patel.1829405.ap@gmail.com>
2022-11-09 21:05:27 +0530

    Added QR Code Functionality

--- src/Pages/DashBoard/DashBoardPage.js
+++ src/Pages/DashBoard/DashBoardPage.js
@@ -1,11 +1,13 @@
-import { React, useState } from 'react'
+import { React, useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { motion } from 'framer-motion'
+import QRCode from 'qrcode'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import 'react-toastify/dist/ReactToastify.min.css';
 import RestartAltRoundedIcon from '@mui/icons-material/RestartAltRounded';
 import CopyAllRoundedIcon from '@mui/icons-material/CopyAllRounded';
+import FileDownloadRoundedIcon from '@mui/icons-material/FileDownloadRounded';
 
 import './DashBoardPage.css'
 import Loading from '../Loading/Loading'
@@ -13,6 +15,7 @@ import { resetViewCount } from '../../store/action/action';
 
 const DashBoardPage = props => {
     const [loading, setLoading] = useState(false)
+    const [qrCode, SetQrCode] = useState('')
     const dispatch = useDispatch()
 
     const UserData = useSelector(state => state.userData)
@@ -26,6 +29,10 @@ const DashBoardPage = props => {
     } else {
         GreetingsText = "Good evening"
     }
+
+    useEffect(() => {
+        QRCodeGenerator(window.location.href + "resume/" + props.id)
+    }, [])
 
     const ContainerVariant = {
         hidden: {
@@ -76,11 +83,21 @@ const DashBoardPage = props => {
     const ResetViewCountHandler = async () => {
         try {
             setLoading(true)
-            await dispatch(resetViewCount({id: props.id}))
+            await dispatch(resetViewCount({ id: props.id }))
             setLoading(false)
         } catch (error) {
             console.log(error)
         }
+    }
+
+    const QRCodeGenerator = (InputText) => {
+        QRCode.toDataURL(InputText, {
+            width: '105%',
+            margin: 1,
+        }, (err, InputText) => {
+            if (err) return console.error(err)
+            SetQrCode(InputText)
+        })
     }
 
     return (
@@ -102,9 +119,10 @@ const DashBoardPage = props => {
                             <motion.p variants={ContainerVariant} className='dashboardpage-viewcountcontainer-viewtext'>Your Resume Link <div title='copy to clipboard' onClick={CopyToClipboardHandler}><CopyAllRoundedIcon /></div></motion.p>
                             <motion.p variants={ContainerVariant} className='dashboardpage-viewcountcontainer-linktext'><a target="_blank" rel="noreferrer" href={window.location.href + "resume/" + props.id}>View Your Resume</a></motion.p>
                         </motion.div>
-                        {/* <motion.div variants={ContainerVariant} className='dashboardpage-copylinkcontainer'>
-
-                        </motion.div> */}
+                        <motion.div variants={ContainerVariant} className='dashboardpage-copylinkcontainer'>
+                            <motion.p variants={ContainerVariant} className='dashboardpage-viewcountcontainer-viewtext'>QR Code<a title="Download" href={qrCode} download={`${UserData.Details[0]?.Name} QR Code.png`} ><FileDownloadRoundedIcon /></a></motion.p>
+                            <motion.p variants={ContainerVariant} className='dashboardpage-viewcountcontainer-counttext'> <img alt='QRcode' src={qrCode} /></motion.p>
+                        </motion.div>
                     </motion.div>
                 </motion.div>
             </div>
